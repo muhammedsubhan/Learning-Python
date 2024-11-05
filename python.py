ca07b772824950434bd1bf083dcd9b6eeabf4cbf
muhammad subhan <muhammedsubhan692@gmail.com>
2024-11-06 00:27:17 +0500

    learning functions

--- python.py
+++ python.py
@@ -21,6 +21,8 @@
 
 # """)
 
+# File Handling 
+
 # import os
 # doc_path =  os.path.dirname(os.path.abspath(__file__))
 # file_path = os.path.join(doc_path, "doc.txt")
@@ -40,3 +42,76 @@
 # with open(file_path, 'a') as file:
 #     file.write("\nhello this an appended text you are seeing.")
 #     file.close()
+
+
+
+
+# Functions
+
+
+# def print_user_details(*details):
+    
+#     name , work, age =  details 
+#     print(f"Hello {name} Oh so your a {work} and your age is {age}")
+
+
+
+
+
+# print_user_details("Muhammad Subhan", "Software Engineer", 23)
+
+# name = ""
+# age=""
+# work=""
+
+
+# def get_user_details():
+#     global name, age,work
+ 
+
+#     name = input("Enter your name " )
+#     age = input("Enter your age ")
+#     work = input("Enter your work " )
+    
+    
+
+
+# def print_values():
+#     print(f"your name is {name} and your age is {age} and you are a {work}")
+    
+    
+    
+    
+# get_user_details()
+# print_values()
+
+
+
+
+import os
+
+dir_name = os.path.dirname(os.path.abspath(__file__))
+persistent_path = os.path.join(dir_name, "doc.txt")
+
+print(persistent_path)
+
+def read_file():
+    
+    with open(persistent_path, "r") as file:
+       content  = file.read()
+       
+       print(content)
+       
+       
+def write_file():
+    with open(persistent_path, "a") as file:
+        file.write("New line of code with functions")   
+       
+       
+       
+       
+    
+       
+       
+read_file()
+write_file()

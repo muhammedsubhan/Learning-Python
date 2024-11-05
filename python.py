830e8d003b1e0ce768034faf560e43b0fe33a3f5
muhammad subhan <muhammedsubhan692@gmail.com>
2024-11-06 00:01:34 +0500

    Learning Python

---
+++ python.py
@@ -0,0 +1,42 @@
+# print("what is your name ? " , end=" ")
+# name = input()
+# print("what is your age ? "  , end=" ")
+# age = input()
+# print("what do you do ? " , end=" ")
+# job = input()
+
+
+# print(f"system do you like {name}")
+# likes = input(">")
+
+
+# print(f"system do you like {name}'s Job")
+# likes_job = input(">")
+
+
+
+# print(f"""
+# so system you said {likes} to me 
+# and i asked you about my job  that you like it or not you said {likes_job}
+
+# """)
+
+# import os
+# doc_path =  os.path.dirname(os.path.abspath(__file__))
+# file_path = os.path.join(doc_path, "doc.txt")
+
+# with open(file_path, "r") as file:
+#     content = file.read()
+#     print("Reading file content:")
+#     print(content)
+ 
+
+
+
+# with open(file_path, 'w') as file:
+#     file.write("hello this is now a new file ")
+
+
+# with open(file_path, 'a') as file:
+#     file.write("\nhello this an appended text you are seeing.")
+#     file.close()

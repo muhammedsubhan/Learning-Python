9086c2de1421dc3b70e0e7eef23ba55ac0fc4617
muhammad subhan <muhammedsubhan692@gmail.com>
2024-11-07 04:58:57 +0500

    for loops and list

--- python.py
+++ python.py
@@ -192,41 +192,69 @@
 
 
 
-print("""You enter a dark room with two doors.
-Do you go through door #1 or door #2?""")
+# print("""You enter a dark room with two doors.
+# Do you go through door #1 or door #2?""")
 
-door = input("> ")
+# door = input("> ")
 
-if door == "1":
-    print("There's a giant bear here eating a cheesecake.")
-    print("What do you do?")
-    print("1. Take the cake.")
-    print("2. Scream at the bear.")
+# if door == "1":
+#     print("There's a giant bear here eating a cheesecake.")
+#     print("What do you do?")
+#     print("1. Take the cake.")
+#     print("2. Scream at the bear.")
     
-    bear = input("> ")
+#     bear = input("> ")
     
-    if bear == "1":
-        print("The bear eats your face off. Good job!")
-    elif bear == "2":
-        print("The bear eats your legs off. Good job!")
-    else:
-        print(f"Well, doing {bear} is probably better.")
-        print("Bear runs away.")
+#     if bear == "1":
+#         print("The bear eats your face off. Good job!")
+#     elif bear == "2":
+#         print("The bear eats your legs off. Good job!")
+#     else:
+#         print(f"Well, doing {bear} is probably better.")
+#         print("Bear runs away.")
 
-elif door == "2":
-    print("You stare into the endless abyss at Cthulhu's retina.")
-    print("1. Blueberries.")
-    print("2. Yellow jacket clothespins.")
-    print("3. Understanding revolvers yelling melodies.")
+# elif door == "2":
+#     print("You stare into the endless abyss at Cthulhu's retina.")
+#     print("1. Blueberries.")
+#     print("2. Yellow jacket clothespins.")
+#     print("3. Understanding revolvers yelling melodies.")
     
-    insanity = input("> ")
+#     insanity = input("> ")
     
-    if insanity == "1" or insanity == "2":
-        print("Your body survives powered by a mind of jello.")
-        print("Good job!")
-    else:
-        print("The insanity rots your eyes into a pool of muck.")
-        print("Good job!")
+#     if insanity == "1" or insanity == "2":
+#         print("Your body survives powered by a mind of jello.")
+#         print("Good job!")
+#     else:
+#         print("The insanity rots your eyes into a pool of muck.")
+#         print("Good job!")
 
-else:
-    print("You stumble around and fall on a knife and die. Good job!")
+# else:
+#     print("You stumble around and fall on a knife and die. Good job!")
+
+
+# Loops and Lists
+
+
+# counts = [1,2,3,4, 5, 6, 7,8 ,9, 10]
+
+
+# for count in counts:
+#     print(f" {count}")
+     
+     
+     
+# fruits = ["Apple", "Banana", "Oranges", "Peaches"]
+
+
+# for fruit in fruits:
+#     print(fruit)
+
+
+# elements = []
+
+# for element in range(1, 11):
+#     elements.append(element)
+
+
+# for e in elements:
+#     print(e)

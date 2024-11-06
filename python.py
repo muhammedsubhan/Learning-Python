5ac305de0132e2fc43281a33f448d68919e75ef1
muhammad subhan <muhammedsubhan692@gmail.com>
2024-11-07 01:04:55 +0500

    functions

--- python.py
+++ python.py
@@ -158,3 +158,75 @@
 # words_sorted = sort_words()
 
 # print(words_sorted)
+
+
+# IF Else Conditions
+
+
+# people = 30
+# cats = 20
+# dogs = 10
+
+# if people > cats: 
+#     print("the world is dry")
+    
+# if people < cats:
+#     print("too many cats the world is doomed")
+    
+# if cats > dogs:
+#     print("too many cats the Dogs are in danger") 
+    
+    
+    
+# if cats == dogs:
+#     print("too many cats")
+    
+# elif dogs < cats:
+#     print("dogs are less")
+    
+# elif dogs != cats:
+#     print("dogs are less")
+    
+# else: 
+#     print("cannot decide")
+
+
+
+print("""You enter a dark room with two doors.
+Do you go through door #1 or door #2?""")
+
+door = input("> ")
+
+if door == "1":
+    print("There's a giant bear here eating a cheesecake.")
+    print("What do you do?")
+    print("1. Take the cake.")
+    print("2. Scream at the bear.")
+    
+    bear = input("> ")
+    
+    if bear == "1":
+        print("The bear eats your face off. Good job!")
+    elif bear == "2":
+        print("The bear eats your legs off. Good job!")
+    else:
+        print(f"Well, doing {bear} is probably better.")
+        print("Bear runs away.")
+
+elif door == "2":
+    print("You stare into the endless abyss at Cthulhu's retina.")
+    print("1. Blueberries.")
+    print("2. Yellow jacket clothespins.")
+    print("3. Understanding revolvers yelling melodies.")
+    
+    insanity = input("> ")
+    
+    if insanity == "1" or insanity == "2":
+        print("Your body survives powered by a mind of jello.")
+        print("Good job!")
+    else:
+        print("The insanity rots your eyes into a pool of muck.")
+        print("Good job!")
+
+else:
+    print("You stumble around and fall on a knife and die. Good job!")
